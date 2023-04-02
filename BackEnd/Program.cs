9b5b81c8a65abbab50a34f8de7620a2eb927855a
Peter Aryanto <peter.aryanto@gmail.com>
2023-04-03 02:53:37 +1000

    Fixed Swagger's unexpected API patch action documentation

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers()
   });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s => s.DocumentFilter<JsonPatchDocumentFilter>());
 
 var app = builder.Build();
 
